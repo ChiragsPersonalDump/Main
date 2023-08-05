0b4d7784d81851e8093ee6dc47a8a6de7e182a37
Chirag Sharma <itschirag21@gmail.com>
2023-08-05 19:00:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let aboutus = document.getElementById("About");
+aboutus.addEventListener("click", ()=>{
+  window.open("http://127.0.0.1:5500/Main/aboutus.html","_self")
+})
+let homepage = document.getElementById("sitename").addEventListener("click",()=>{
+  window.open("http://127.0.0.1:5500/Main/index.html","_self")
+})
+let contact = document.getElementById("contact").addEventListener("click",()=>{
+  window.open("http://127.0.0.1:5500/Main/contact.html","_self")
+})
+let insta = document.getElementById("insta").addEventListener("click",()=>{
+  window.open("https://www.instagram.com/_x.chirag.7__/","_blank")
+})
+let twitter = document.getElementById("twitter").addEventListener("click",()=>{
+  window.open("https://twitter.com/DrogonTargeryan","_blank")
+})
+let linkin = document.getElementById("linkedin").addEventListener("click",()=>{
+  window.open("https://www.linkedin.com/in/chirag-sharma-93058a24a/","_blank")
+})
+let youtube = document.getElementById("youtube").addEventListener("click",()=>{
+  window.open("https://www.youtube.com/channel/UCxqQEvRcqpgOpz5tS_QN3Dg","_blank")
+})
+let python = document.getElementById("btnpython").addEventListener("click",()=>{
+  window.open("http://www.youtube.com","_self")
+})
+
+
